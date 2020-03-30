8471b3e8d0a43105f1d92a96df1c64b07d226ed0
Markburt <wangye175005@163.com>
2020-03-30 22:40:33 +0800

    Authentication encryption client

---
+++ Development_environment_construction/SpringCloud_Eureka_Environment/Eureka_OAuth_Attestation/OAuth_Cient/src/main/java/com/springboot/cloud/auth/client/config/FeignOkHttpConfig.java
@@ -0,0 +1,43 @@
+package com.springboot.cloud.auth.client.config;
+
+import feign.Feign;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author Markburt
+ * @Description :
+ * 需要修改成OKHTTP的客户端，需要在配置文件增加
+ *     feign.httpclient.enabled=false
+ * 	   feign.okhttp.enabled=true
+ * @DATE: 2020/3/30
+ * @TIME: 下午 10:35
+ * @YEAR: 2020
+ * @Version :
+ **/
+
+@AutoConfigureBefore(FeignAutoConfiguration.class)
+@Configuration
+@ConditionalOnClass(Feign.class)
+public class FeignOkHttpConfig {
+
+    private int feignOkHttpReadTimeout = 60;
+    private int feignConnectTimeout = 60;
+    private int feignWriteTimeout = 120;
+
+    @Bean
+    public okhttp3.OkHttpClient okHttpClient() {
+        return new okhttp3.OkHttpClient.Builder()
+                .readTimeout(feignOkHttpReadTimeout, TimeUnit.SECONDS)
+                .connectTimeout(feignConnectTimeout, TimeUnit.SECONDS)
+                .writeTimeout(feignWriteTimeout, TimeUnit.SECONDS)
+//				.connectionPool(new ConnectionPool(int maxIdleConnections, long keepAliveDuration, TimeUnit timeUnit))   //自定义链接池
+//				.addInterceptor(XXXXXXXInterceptor) 	//自定义拦截器
+                .build();
+    }
+}
